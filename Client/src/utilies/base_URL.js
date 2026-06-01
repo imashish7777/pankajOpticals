de67a673984aaa11ad6b6fd2eced9304f355dce0
Ashish chandra <144109822+imashishchandra777@users.noreply.github.com>
2026-06-01 16:26:18 +0530

    Update project

--- Client/src/utilies/base_URL.js
+++ Client/src/utilies/base_URL.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = (
-  process.env.REACT_APP_API_URL || "http://localhost:3001"
+  process.env.REACT_APP_API_URL || "https://pankajopticals.onrender.com"
 ).replace(/\/$/, "");
 
 export const base_URL = `${API_BASE_URL}/`;
