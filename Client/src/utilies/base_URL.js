aff771cb9c0b05d76f07a0043b396ddc49ff7cac
Ashish chandra <144109822+imashishchandra777@users.noreply.github.com>
2026-06-08 14:51:52 +0530

    migrate to vite

--- Client/src/utilies/base_URL.js
+++ Client/src/utilies/base_URL.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = (
-  process.env.REACT_APP_API_URL || "https://pankajopticals.onrender.com"
+  import.meta.env.VITE_API_URL || "https://pankajopticals.onrender.com"
 ).replace(/\/$/, "");
 
 export const base_URL = `${API_BASE_URL}/`;
